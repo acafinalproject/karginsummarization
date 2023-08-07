a29558eb4915802e5d4d7780f08bd2668c0506fe
Mkrtchyan03 <gagik.krtchyan.1234@gmail.com>
2023-08-07 16:16:17 +0400

    fixed

--- data_tokenizer.py
+++ data_tokenizer.py
@@ -6,10 +6,3 @@ from transformers import AutoTokenizer
 dataset = load_dataset('multi_news')
 tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-xsum')
 
-#print(dataset)
-
-#train_ds = dataset['train']
-#train_ds, val_ds, test_ds = dataset['train'], dataset['validation'], dataset['test']
-#print(type(test_ds))
-#print(d, 'document')
-#print(s, 'summary'))
