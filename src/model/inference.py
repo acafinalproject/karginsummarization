b0bc50d2f479d21fbdfa4b81097b8e517ee431d2
Khachatryan <72154923+Artyom-Khachatryan@users.noreply.github.com>
2023-08-18 15:55:58 +0400

    Update inference.py

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,11 +1,20 @@
-from transformers import AutoTokenizer
-from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+# Get input text from the user
 text = input('Enter text to summarize: ')
+
+# Prepend "summarize: " to the input text
 text = 'summarize: ' + text
+
+# Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained("GagMkrtchyan/T5")
+
+# Tokenize the input text
 inputs = tokenizer(text, return_tensors="pt").input_ids
 
-
+# Load the T5 model for sequence-to-sequence tasks
 model = AutoModelForSeq2SeqLM.from_pretrained("GagMkrtchyan/T5")
+
+# Generate a summary using the model
 outputs = model.generate(inputs, max_new_tokens=100, do_sample=False)
+
