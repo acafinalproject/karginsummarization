801f74bf50f70e7f532a79d731e5428f290b95c7
Khachatryan <72154923+Artyom-Khachatryan@users.noreply.github.com>
2023-08-18 15:54:05 +0400

    Update inference.py

--- src/model/inference.py
+++ src/model/inference.py
@@ -3,9 +3,9 @@ from transformers import AutoModelForSeq2SeqLM
 
 text = input('Enter text to summarize: ')
 text = 'summarize: ' + text
-tokenizer = AutoTokenizer.from_pretrained("stevhliu/my_awesome_billsum_model")
+tokenizer = AutoTokenizer.from_pretrained("GagMkrtchyan/T5")
 inputs = tokenizer(text, return_tensors="pt").input_ids
 
 
-model = AutoModelForSeq2SeqLM.from_pretrained("stevhliu/my_awesome_billsum_model")
+model = AutoModelForSeq2SeqLM.from_pretrained("GagMkrtchyan/T5")
 outputs = model.generate(inputs, max_new_tokens=100, do_sample=False)
